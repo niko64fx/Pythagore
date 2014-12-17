2cc1a3cc7d89e70fa679eb12824c6dee1cb6e677
Ivan Gabriele <ivan.gabriele@gmail.com>
2014-12-17 11:43:51 +0100

    controller + localStorage

---
+++ js/controller/blocksController.js
@@ -0,0 +1,26 @@
+block.controller(
+	'blocksCtrl',
+	['$scope', 'localStorage'],
+	function ($scope, localStorage)
+	{
+		if (localStorage.load('blocks') === null) localStorage.save('blocks', []);
+		$scope.blocks = localStorage.load('blocks');
+
+		$scope.block = {
+			gridX: 0,
+			gridY: 0,
+			area: 0,
+			value: 1,
+			isDraggable: false,
+			isDroppable: false,
+			isShownNumber: false,
+			isShownImage: false
+		}
+
+		$scope.addBlock = function()
+		{
+			$scope.blocks.push($scope.block);
+			localStorage.save('blocks', $scope.blocks);
+		}
+	}
+);
